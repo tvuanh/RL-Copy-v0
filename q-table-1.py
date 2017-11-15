3d404253c5142765c81f040bac7ab1aa53be8762
Vu Anh Tuan <tuan.vu-anh@polytechnique.org>
2017-11-15 14:53:25 +0100

    Removed functions encode_action and decode_action from q-table-1.py.

--- q-table-1.py
+++ q-table-1.py
@@ -9,43 +9,7 @@ import numpy as np
 
 import gym
 
-
-def encode_action(action):
-    """
-    >>> encode_action((0, 0, 0))
-    0
-    >>> encode_action((1, 0, 0))
-    1
-    >>> encode_action((1, 1, 0))
-    3
-    >>> encode_action((1, 1, 1))
-    7
-    >>> encode_action((1, 1, 4))
-    19
-    >>> encode_action((1, 0, 4))
-    17
-    """
-    first, second, third = action
-    return first + 2 * second + 4 * third
-
-
-def decode_action(action):
-    """
-    >>> decode_action(0)
-    (0, 0, 0)
-    >>> decode_action(1)
-    (1, 0, 0)
-    >>> decode_action(2)
-    (0, 1, 0)
-    >>> decode_action(4)
-    (0, 0, 1)
-    >>> decode_action(9)
-    (1, 0, 2)
-    """
-    third = action // 4
-    second = (action - 4 * third) // 2    
-    first = action - 4 * third - 2 * second
-    return (first, second, third)
+from utils import encode_action, decode_action
 
 
 class CopyQTable(object):
@@ -78,9 +42,6 @@ class CopyQTable(object):
 
 
 if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
-
     env = gym.make('Copy-v0')
 #     env = gym.wrappers.Monitor(env, '/tmp/copy-v0', force=True)
 
